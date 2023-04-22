148f35730d11a77739ffc4ec18b79cf2ca527f4a
Ana Clara Saraiva <120690180+saraivana@users.noreply.github.com>
2023-04-22 15:27:23 -0300

    Update and rename somaelem.py to Função_soma_elementos.py

---
+++ Função_soma_elementos.py
@@ -0,0 +1,10 @@
+#Recebe como pârametro uma lista com números inteiros e devolve um número inteiro correspondente à soma dos elementos da lista recebida.
+
+def soma_elementos(lista):
+
+    soma = 0
+
+    for i in lista:
+        soma +=i
+
+    return soma
--- somaelem.py
+++
@@ -1,8 +0,0 @@
-def soma_elementos(lista):
-
-    soma = 0
-
-    for i in lista:
-        soma +=i
-
-    return soma
