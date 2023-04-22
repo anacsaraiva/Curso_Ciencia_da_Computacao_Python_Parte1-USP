df033fe022e43f37e1e08576cf85e2e3b4bd0f0b
Ana Clara Saraiva <120690180+saraivana@users.noreply.github.com>
2023-04-22 15:04:01 -0300

    Update and rename vogais.py to Função_vogais.py

--- Função_vogais.py
+++ Função_vogais.py
@@ -1,3 +1,5 @@
+#Função vogal que recebe um único caractere como parâmetro e devolde True se ele for um vogal e False se for uma consoante.
+
 def vogal (x):
     if x=="a" or x =="e" or x=="i" or x== "o" or x== "u" or x=="A" or x =="E" or x=="I" or x== "O" or x== "U":
         return True
