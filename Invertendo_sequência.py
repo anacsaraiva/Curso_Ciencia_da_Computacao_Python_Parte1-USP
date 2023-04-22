949f48537efc86b1a78ae7341e599d0ddb536801
Ana Clara Saraiva <120690180+saraivana@users.noreply.github.com>
2023-04-22 15:31:29 -0300

    Update and rename invertendo.py to Invertendo_sequência.py

--- Invertendo_sequência.py
+++ Invertendo_sequência.py
@@ -1,3 +1,4 @@
+#Recebe uma sequência de números inteiros e imprime todos os valores em ordem inversa. A sequência deve terminar pelo número 0. Note que 0 não deve fazer parte da sequência.
 
 recebe = 1
 lista = []
